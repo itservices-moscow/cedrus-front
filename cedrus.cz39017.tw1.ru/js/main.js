43db7db1c7851a8df83baad29f99bce31a4492d0
andand44 <andrey9991@gmail.com>
2023-08-25 17:19:00 +0300

    add product-color

--- cedrus.cz39017.tw1.ru/js/main.js
+++ cedrus.cz39017.tw1.ru/js/main.js
@@ -1,29 +1,46 @@
-$(document).ready(function() {
+$(document).ready(function () {
+    // product counter
     updateMinusButtonState();
 
-  $('body').on('click', '.product-item__quanti button', function() {
-    var input = $(this).siblings('input');
-    var currentValue = parseInt(input.val());
+    $('body').on('click', '.product-item__quanti button', function () {
+        var input = $(this).siblings('input');
+        var currentValue = parseInt(input.val());
 
-    if ($(this).text() === '+') {
-      input.val(currentValue + 1);
-    } else if ($(this).text() === '-' && currentValue > 1) {
-      input.val(currentValue - 1);
+        if ($(this).text() === '+') {
+            input.val(currentValue + 1);
+        } else if ($(this).text() === '-' && currentValue > 1) {
+            input.val(currentValue - 1);
+        }
+
+        updateMinusButtonState();
+    });
+
+    function updateMinusButtonState() {
+        $('.product-item__quanti').each(function () {
+            var input = $(this).find('input');
+            var minusButton = $(this).find('button:contains("-")');
+
+            if (input.val() <= 1) {
+                minusButton.prop('disabled', true);
+            } else {
+                minusButton.prop('disabled', false);
+            }
+        });
     }
 
-    updateMinusButtonState();
-  });
+    // product color
+    var originalText = $('.product-color__all').text();
 
-  function updateMinusButtonState() {
-    $('.product-item__quanti').each(function() {
-      var input = $(this).find('input');
-      var minusButton = $(this).find('button:contains("-")');
+    $('body').on('click', '.product-color__all', function () {
+        var link = $(this);
+        var productColor = $('.product-color');
 
-      if (input.val() <= 1) {
-        minusButton.prop('disabled', true);
-      } else {
-        minusButton.prop('disabled', false);
-      }
+        productColor.toggleClass('show-more');
+
+        if (productColor.hasClass('show-more')) {
+            link.text(link.attr('data-text'));
+        } else {
+            link.text(originalText);
+        }
     });
-  }
-  });
+});

bbf1c6f17a3fe089bc08a4380d2470d73debb165
andand44 <andrey9991@gmail.com>
2023-08-25 11:16:45 +0300

    fix counter, brands

--- cedrus.cz39017.tw1.ru/js/main.js
+++ cedrus.cz39017.tw1.ru/js/main.js
@@ -1,3 +1,27 @@
-var swiper = new Swiper("#carouselExampleIndicators .carousel-inner", {
-    slidesPerView: 1
-});
+$(document).ready(function() {
+    updateMinusButtonState();
+
+  $('body').on('click', '.product-item__quanti button', function() {
+    var input = $(this).siblings('input');
+    var currentValue = parseInt(input.val());
+
+    if ($(this).text() === '+') {
+      input.val(currentValue + 1);
+    } else if ($(this).text() === '-' && currentValue > 1) {
+      input.val(currentValue - 1);
+    }
+
+    updateMinusButtonState();
+  });
+
+  function updateMinusButtonState() {
+    var input = $('.product-item__quanti input');
+    var minusButton = $('.product-item__quanti button:contains("-")');
+
+    if (input.val() <= 1) {
+      minusButton.prop('disabled', true);
+    } else {
+      minusButton.prop('disabled', false);
+    }
+  }
+  });
